74ceed4b33f883cc7fc147e7fd4086435223da31
dberg99 <d.bergman@uqconnect.edu.au>
2021-08-12 20:14:59 +1000

    Merge branch 'dustin' of https://github.com/belskiza/Pet-Heroes- into dustin
    # Conflicts:
    #	index.php

---
+++ additional/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName = "pet_heroes";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName, 8888);
+?>
--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<?php
+include_once 'additional/dbh.inc.php'
+?>
 <html>
 <head>
     <title>Pet Heroes</title>
 </head>
 
 <body>
+<?php include_once 'header.php'?>
 <h1>Pet Heroes</h1>
+<?php
+    $sql = "SELECT * FROM users;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0 ){
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['first_name'];
+        }
+    }
+?>
+
 </body>
+
 </html>
