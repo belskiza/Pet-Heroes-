c5a052e52d60e5d71b9049f5029f1df76e77f11b
TOMpetheroes <t.trebilcock@uqconnect.edu.au>
2021-08-12 17:25:51 +1000

    Navagation bar template

--- index.php
+++ index.php
@@ -1,9 +1,56 @@
 <html>
 <head>
     <title>Pet Heroes</title>
-</head>
 
+
+    <style>
+        body {margin:0;}
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #04AA6D;
+        }
+    </style>
+
+
+</head>
 <body>
+
+<ul>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#news">Log in</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
+
+<br>
+<br>
 <h1>Pet Heroes</h1>
 
 <h1>this is TOms test here</h1>
