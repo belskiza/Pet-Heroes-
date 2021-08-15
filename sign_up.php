ebf744124dd9a37478afa0dca969aacd48a100a2
dberg99 <d.bergman@uqconnect.edu.au>
2021-08-15 13:48:01 +1000

    Merge branch 'main' of https://github.com/belskiza/Pet-Heroes- into main
    # Conflicts:
    #	index.php

--- sign_up.php
+++ sign_up.php
@@ -26,16 +26,16 @@ include_once 'additional/dbh.inc.php'
         </div>
     </div>
     <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label" ">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label" ">Username</label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Username">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label" ">Password</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Password">
-    </div>
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label" ">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
     </div> <br/>
     <div class="mb-3">
         <div class="row g-3">
