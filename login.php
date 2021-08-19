226cb57de5172f5e5634eedf03db72d01c70c1e1
dberg99 <d.bergman@uqconnect.edu.au>
2021-08-19 15:54:38 +1000

    Merge branch 'dustin' into main
    # Conflicts:
    #	sign_up.php

--- login.php
+++ login.php
@@ -2,10 +2,9 @@
 <head>
     <title>Pet Heroes</title>
 </head>
-
 <body >
+<img src="files/background.jpeg" style="position: fixed; filter: blur(20px) ; width: 105%; margin: -5%">
 <?php include_once 'header.php'?>
-<img src=”background.jpeg” alt=”Image”>.
 <!-- Container for sign up form -->
 <div class="alert alert-secondary col-md-4" style="margin:auto; margin-top: 2%; padding: 3%; background-color: whitesmoke; min-width: 400pt">
     <form action="additional/login.inc.php" method="post">
