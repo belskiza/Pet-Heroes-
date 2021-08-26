c79aadd166e4df412bd3aa9de8f10c1812972469
lochlan-hill <mail.lochlan@gmail.com>
2021-08-26 11:35:34 +1000

    Improved Profile Page

---
+++ setup_preferences2.php
@@ -0,0 +1,39 @@
+<html>
+
+<?php include_once 'header.php'?>
+
+<style>
+    .down-three {
+        margin-bottom: 3cm;
+    }
+</style>
+
+<style>
+    .down-one {
+        margin-bottom: 1cm;
+    }
+</style>
+
+<p class='down-one'>
+</p>
+<div class="progress">
+        <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar"
+             aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 60%"></div>
+    </div> <br/>
+<body>
+<h1> Setup Preferences </h1> 
+
+
+<p class='down-three'>
+</p>
+
+Next set of questions
+
+<p class='down-three'>
+</p>
+
+<p class='down-three'>
+</p>
+
+<a class="btn btn-primary btn-lg" href="setup_preferences1.php"> Previous Page </a>
+<a class="btn btn-primary btn-lg" href="setup_preferences2.php"> Next Page </a>
