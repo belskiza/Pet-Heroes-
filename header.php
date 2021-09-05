7bdf8797ce7393dd99f09a8621d6dba4cab6921a
belskiza <bakerisabellarose@gmail.com>
2021-09-05 20:06:00 +1000

    Edited Nav bar Changed account page to match high fidelity Edited landing page image Added about page

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <link rel="stylesheet" href="css/bootstrap.css">
     <?php
@@ -7,33 +9,50 @@
     <link rel="stylesheet" href="css/style.css">
     <style>
         .topnav{
-            background-color: limegreen;
+            background-color: white;
+            width: 100%;
         }
+        .topnav a h3{
+            color: black;
+            font-family: "Chelsea Market";
+            font-size: 1.5rem;
+            position: center;
+            margin-top: 30px;
+            margin-right: 100px;
+        }
+        .topnav img {
+            width: 80px;
+            height: 80px;
+        }
+        .topnav {
+            border-bottom: 3px solid #BCE76D;
+        }
+
     </style>
 </head>
 
 <body> <!-- logo placeholder -->
 
 <!-- NAVBAR -->
-
-<div class="topnav">
+<div class=" topnav navbar navbar-expand-lg navbar-light">
+    <div class="navbar-nav justify-content-between w-100">
     <?php if (isset($_SESSION['username'])){
-        echo "<a href='profile.php'?>".$_SESSION['first_name']."</a>";
-        echo "<a href='home.php'?>Home</a>";
-        echo "<a href='additional/logout.inc.php'?>Log Out</a>";
-        echo "<a href='#about' ?>PLACEHOLDER</a>";
+        echo "<a href='landing_page.php' ><img src='Files/logo.png'></a>";
+        echo "<a href='landing_page.php'><h3 style='font-size: 1.5vw'>Home</h3></a>";
+        echo "<a href='home.php'><h3 style='font-size: 1.5vw'>Matches</h3></a>";
+        echo "<a href='home.php'><h3 style='font-size: 1.5vw'>Upload</h3></a>";
+        echo "<a href='account.php'><h3 style='font-size: 1.5vw'>Account</h3></a>";
+
 
     } else {
-        echo "<a href='sign_up.php'?> Sign Up </a>
+        echo "<a href='sign_up.php'> Sign Up </a>
               <a href='login.php'?>Log In</a>";
         echo "<a href='#about' ?>PLACEHOLDER</a>";
 
     }
     ?>
-</div>
-
-
-
+    </div>
 
 </div>
+
 </body>
--- landing_page.php
+++ landing_page.php
@@ -12,26 +12,24 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js'></script>
-    <?php include_once 'header.php'?>
 
     <style>
-        .col-sm-4 {
-            margin-top: 22%;
-        }
         .col-sm-4 h1 {
             font-size: 7vw;
             color: #FDE9AA;
             font-family: "Chelsea Market";
+            margin-left: -20%;
         }
         .container {
-            margin-left: 4%;
+            margin-top: 30%;
         }
 
     </style>
 </head>
 
 <body>
-<img src="Files/image.jpg" style="position: fixed;  width: 105%; margin: -5%">
+<?php include_once 'header.php'?>
+<img src="Files/image%208.png" style="position: fixed;  width: 105%; margin-top: 5%;">
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
