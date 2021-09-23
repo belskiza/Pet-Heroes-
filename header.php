e8a84000744411f78a5cd3399507d54ccc6c3c08
dkhoo <dkhoo.2001@gmail.com>
2021-09-23 17:08:16 +1000

    typo

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
 <body>
 <?php if (isset($_SESSION['username'])){ ?>
     <nav class='navbar sticky-top navbar-expand-lg navbar-light bg-light'>
-        <a href='landing_page.php' ><img src='Files/logo.png' style='width: 70px; margin-left: 5%'></a>
+        <a href='landing_page.php' ><img src='files/logo.png' style='width: 70px; margin-left: 5%'></a>
                 <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
     <span class='navbar-toggler-icon'></span>
   </button>
@@ -78,7 +78,7 @@
 <?php
 } else {
     echo "<nav class='navbar sticky-top navbar-expand-lg navbar-light bg-light'>
-                <a href='landing_page.php' ><img src='Files/logo.png' style='width: 70px; margin-left: 5%'></a>
+                <a href='landing_page.php' ><img src='files/logo.png' style='width: 70px; margin-left: 5%'></a>
                 <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
     <span class='navbar-toggler-icon'></span>
   </button>
