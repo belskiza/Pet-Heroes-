91db47202df2cb7b08947126fdaf667867433c98
dennis1269 <dkhoo.2001@gmail.com>
2021-10-27 22:29:10 +1000

    header logo for uqcloud

--- additional/dbh.inc.php
+++ additional/dbh.inc.php
@@ -5,6 +5,8 @@ $dbPassword = "root";
 $dbName = "pet_heroes";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName, 8889);
+$conn = new PDO("mysql:host=localhost;dbname=pet_heroes", $dbPassword, $dbName);
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
