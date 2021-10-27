ddba6569ce55cce63b86ec983ab0119cbdc68a47
belskiza <bakerisabellarose@gmail.com>
2021-10-27 16:01:58 +1000

    Changed index to include who are are and what our goal is

--- index.php
+++ index.php
@@ -70,15 +70,8 @@
     <div class="container text">
         <h1 class="modal-title">
             <b>What are we? </b> <hr/>
-        </h1>git
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tristique diam. Ut id auctor magna,
-            nec blandit nunc. Phasellus faucibus interdum purus, eget auctor nulla bibendum lacinia. Sed lobortis sagittis urna a tempor.
-            Morbi et nunc sit amet odio malesuada sollicitudin. Integer fermentum a massa eget finibus. Proin non facilisis ante.
-            Sed dictum augue elementum, malesuada purus sit amet, consectetur tortor. Morbi consequat diam ex, in faucibus tellus
-            iaculis sit amet. Curabitur cursus eu ligula a tempus. Phasellus sit amet finibus sapien. Suspendisse laoreet, urna a
-            interdum molestie, odio nibh lobortis ante, ac ullamcorper elit eros sed ipsum. Aenean varius magna diam, in aliquet
-            purus dapibus et. Suspendisse id iaculis sapien. Curabitur pretium scelerisque magna a luctus. Donec id imperdiet ipsum,
-            non condimentum est. </h1>
+        </h1>
+        <h1>We are Pet Heroes! Started by a group of university friends, Pet Heroes wants to help stray pets, abandoned pets, and any pet seeking a home find a home. A forever home. So we aim to match ‘Re-homers’ with prospective adopters so that we can reduce, and hopefully one day, eradicate the issue of un-homed pets. We also hope to relieve the stress of both adopters making sure that their pet has an ideal and safe home, and the stress of adoptees making sure they have found the right fit for their family.  </h1>
     </div>
 
     <div class="parallax-window" data-parallax="scroll" data-image-src="files/landing_image_2.jpeg" data-z-index="1" data-position="0 -200"></div>
@@ -87,14 +80,7 @@
         <h1 class="modal-title">
             <b>Our Goal? </b> <hr/>
         </h1>
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tristique diam. Ut id auctor magna,
-            nec blandit nunc. Phasellus faucibus interdum purus, eget auctor nulla bibendum lacinia. Sed lobortis sagittis urna a tempor.
-            Morbi et nunc sit amet odio malesuada sollicitudin. Integer fermentum a massa eget finibus. Proin non facilisis ante.
-            Sed dictum augue elementum, malesuada purus sit amet, consectetur tortor. Morbi consequat diam ex, in faucibus tellus
-            iaculis sit amet. Curabitur cursus eu ligula a tempus. Phasellus sit amet finibus sapien. Suspendisse laoreet, urna a
-            interdum molestie, odio nibh lobortis ante, ac ullamcorper elit eros sed ipsum. Aenean varius magna diam, in aliquet
-            purus dapibus et. Suspendisse id iaculis sapien. Curabitur pretium scelerisque magna a luctus. Donec id imperdiet ipsum,
-            non condimentum est. </h1>
+        <h1>Our goal is to reduce the amount of animals killed due to over whelmed shelters by 20% by the year 2025. Every year in Australia animals with so much love and potential are needlessly euthanised due to shelters across the country being overrun, overfilled and understaffed. They need our help! So we hope that by streamlining the adoption process we can reduce the amount of pets needing a home and stop preventable euthanasia. There is also an economic benefit in reducing the amount of stray animals, animal shelters would have a lesser burden and council’s would not have to spend as much time to rescue stray animals. Online partner matching services have had such great success, so why not meet the new love of your life today with Pet Heroes. Because they are more than pets. They are family. </h1>
     </div>
 
     <div class="parallax-window" data-parallax="scroll" data-image-src="files/landing_image_3.jpeg" data-z-index="1" data-position="0 -200"></div>
